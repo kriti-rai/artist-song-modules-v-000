c3a7bdbbe3bdda0ebb7997a742ad28cfdaffe95d
Kriti Rai <kriti.rai@gmail.com>
2018-02-15 01:21:12 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,8 +2,10 @@ require 'pry'
 
 class Artist
   extend Memorable::ClassMethods
+  include Memorable::InstanceMethods
   extend Findable
   include Paramable
+  
 
   attr_accessor :name
   attr_reader :songs
