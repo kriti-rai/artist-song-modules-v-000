3024cabb6dc1026986f4b025ab1ccca8d24e0dc4
Kriti Rai <kriti.rai@gmail.com>
2018-02-15 01:22:13 +0000

    automatically backed up by learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -3,9 +3,14 @@ module Memorable
     def reset_all
       all.clear
     end
-
     def count
       all.count
+    end
+  end
+
+  module InstanceMethods
+    def initialize
+      self.class.all << self
     end
   end
 end
